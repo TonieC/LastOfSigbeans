7b2612dfe2b92ffd10152f84a11c40d31aa46fe7
TonieC <jesstonievlogs@gmail.com>
2026-02-04 19:32:11 +0800

    WIP: saving current changes before rollback

--- Login/Form1.vb
+++ Login/Form1.vb
@@ -1,10 +1,34 @@
-﻿Public Class Form1
-<<<<<<< HEAD
+﻿Imports System.Data.OleDb
+Imports System.Data
+Public Class Form1
+    Dim connect As New OleDbConnection
+    Dim command As OleDbCommand
+    Dim sql As String = Nothing
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
-        Form2.Show
         Me.Hide()
+        Form2.Show()
     End Sub
-=======
-
->>>>>>> 1be264a6d1e4f9a42075fe4c47f064fd9766dae1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connect.ConnectionString = & quot;Provider=Microsoft.ACE.OLEDB.12.0;Data
+Source = C : \Users\sirtr\Downloads\mercurydb.accdb&quot;
+End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If connect.State = ConnectionState.Closed Then
+            connect.Open()
+            If TextBox1.Text = Nothing Or TextBox2.Text = Nothing Then
+                MsgBox(& quot;Please enter your username And password&quot;, MsgBoxStyle.Exclamation, & quot;Input Required&quot;)
+            End If
+        Else
+            sql = & quot;Select COUNT(*) FROM [user] WHERE username = ? And password = ?&quot;
+command = New OleDbCommand(sql, connect)
+            command.Parameters.AddWithValue(& quot;?&quot;, TextBox1.Text)
+            command.Parameters.AddWithValue(& quot;?&quot;, TextBox2.Text)
+            Dim count As Integer = Convert.ToInt32(command.ExecuteScalar())
+            If count & gt; 0 Then
+MsgBox(& quot;Login Successful&quot;, MsgBoxStyle.Information, & quot;Welcome&quot;)
+            Else
+                MsgBox(& quot;Invalid username Or password&quot;, MsgBoxStyle.Critical, & quot;Login Failed&quot;)
+            End If
+        End If
+    End Sub
 End Class
--- Login/Form2.vb
+++ Login/Form2.vb
@@ -1,6 +1,52 @@
-﻿Public Class Form2
+﻿PuImports System.Data.OleDb
+Imports System.Data
+
+Public Class Form2
+    Dim connect As New OleDbConnection()
+    Dim command As OleDbCommand
+    Dim sql As String = Nothing
+
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
-        Form1.Show()
         Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connect.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\sirtr\Downloads\mercurydb.accdb"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If String.IsNullOrWhiteSpace(TextBox1.Text) OrElse String.IsNullOrWhiteSpace(TextBox2.Text) Then
+            MsgBox("Please enter your username And password", MsgBoxStyle.Exclamation, "Input Required")
+            Return
+        End If
+
+        Try
+            If connect.State = ConnectionState.Closed Then connect.Open()
+
+            sql = "SELECT COUNT(*) FROM [user] WHERE username = ?"
+            command = New OleDbCommand(sql, connect)
+            command.Parameters.Clear()
+            command.Parameters.AddWithValue("?", TextBox1.Text)
+            Dim check As Integer = Convert.ToInt32(command.ExecuteScalar())
+
+            If check > 0 Then
+                MsgBox("Username already exists", MsgBoxStyle.Exclamation, "Duplicate")
+            Else
+                sql = "INSERT INTO [user] ([username], [password]) VALUES (?, ?)"
+                command = New OleDbCommand(sql, connect)
+                command.Parameters.Clear()
+                command.Parameters.AddWithValue("?", TextBox1.Text)
+                command.Parameters.AddWithValue("?", TextBox2.Text)
+                command.ExecuteNonQuery()
+                MsgBox("Sign Up Successful", MsgBoxStyle.Information, "Welcome")
+                TextBox2.Clear()
+                TextBox1.Clear()
+            End If
+        Catch ex As Exception
+            MsgBox("An error occurred: " & ex.Message, MsgBoxStyle.Critical, "Error")
+        Finally
+            If connect.State = ConnectionState.Open Then connect.Close()
+        End Try
     End Sub
 End Class
