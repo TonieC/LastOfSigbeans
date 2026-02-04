26851d3560a3fd0f633016c969eda16899f2c0a5
TonieC <jesstonievlogs@gmail.com>
2026-02-04 18:37:38 +0800

    Added basic Login system Non-FunctionalAdd project files.

---
+++ Login/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Form2.Show
+        Me.Hide()
+    End Sub
+End Class
---
+++ Login/Form2.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        LinkLabel1 = New LinkLabel()
+        Button1 = New Button()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(342, 586)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(127, 15)
+        LinkLabel1.TabIndex = 13
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Log In to your account"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Showcard Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(342, 476)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(130, 38)
+        Button1.TabIndex = 12
+        Button1.Text = "SIGN UP"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Wide Latin", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(316, 383)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(197, 19)
+        Label3.TabIndex = 11
+        Label3.Text = "PASSWORD"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Wide Latin", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(314, 293)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(199, 19)
+        Label2.TabIndex = 10
+        Label2.Text = "USERNAME"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Wide Latin", 48F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(141, 61)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(558, 79)
+        Label1.TabIndex = 9
+        Label1.Text = "SIGN UP"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(309, 315)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(204, 23)
+        TextBox2.TabIndex = 8
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(309, 405)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(204, 23)
+        TextBox1.TabIndex = 7
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(829, 665)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form2"
+        Text = "Form2"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
